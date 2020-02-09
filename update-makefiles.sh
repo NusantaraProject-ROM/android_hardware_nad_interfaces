fc1bf35ee2c271d90236d33719471b50c70c4727
Genkzsz11 <Genkzsz11@gmail.com>
2020-02-09 07:04:30 +0700

    init du

--- update-makefiles.sh
+++ update-makefiles.sh
@@ -3,6 +3,6 @@
 source $ANDROID_BUILD_TOP/system/tools/hidl/update-makefiles-helper.sh
 
 do_makefiles_update \
-  "vendor.lineage:hardware/lineage/interfaces" \
+  "vendor.lineage:hardware/du/interfaces" \
   "android.hardware:hardware/interfaces" \
   "android.hidl:system/libhidl/transport"
